c366db7ec26993c6141f57890d0cdbda0cbbb32c
jabiim <jabiim00@gmail.com>
2022-03-25 23:19:39 +0100

    feat: make it work
    - it works
    - updated to latest buttplug
    - multiple toys
    - add queue handling
    - ramping strength
    - optional levels

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-# Buttplug Tampermonkey Scripts and Utilities
+# Fork of Buttplug Tampermonkey Scripts and Utilities
+
+If you somehow managed to end up in this end of the internet, then I'm happy to tell you that I managed to load the latest buttplug with tampermonkey.
+But for it to work you have to disable a browser security function with a chrome plugin, that prevents the buttplug script to load the wasm using fetch:
+https://chrome.google.com/webstore/detail/always-disable-content-se/ffelghdomoehpceihalcnbmnodohkibj
+
+If you have done that you can install the tampermonkey script:
+
+[Chaturbate Vibrate On Tip](https://github.com/jabiim/buttplug-tampermonkey/raw/master/scripts/chaturbate-tips-buttplug.user.js)
+
+This script does not use the utils anymore, but it has way less of a UI. 
+(But I think the original did not use it either.)
+
+Basically 2 button, `Click to connect device` and `Clear queue, stop device`.
+
+There is 4 thing that is different form the original:
+- it works with the current split view chat window
+- it can handle multiple toys
+- it is no longer goes at full power all the time (it is amount dependent as well)
+  - 200 token or higher is 100%, it will go at least 5% after that it is linearly increasing based on the token amount
+  - configurable check the code 
+- it queues the tips, so you will feel every token not just changing to the last ones
+
+If you want to customize thing open the script and start editing it after you installed it.
+
+There is a relatively easy way to set up levels same as the models, you will find it around the 60th line or so.
+
+## Original content
 
 This repo houses scripts and utilties for using
 [buttplug-js](https://github.com/buttplugio/buttplug-js) (the
--- scripts/chaturbate-tips-buttplug.user.js
+++ scripts/chaturbate-tips-buttplug.user.js
@@ -2,7 +2,7 @@
 // @name Chaturbate Tips to Buttplug.io
 // @description Uses buttplug-js to cause local sex toys to work when a tip appears in a chaturbate chat
 // @author buttplugio
-// @version 0.5
+// @version 0.6
 // @homepage https://buttplugio.github.io/buttplug-tampermonkey
 // @updateurl https://github.com/buttplugio/buttplug-tampermonkey/raw/master/scripts/chaturbate-tips-buttplug.user.js
 // @downloadurl https://github.com/buttplugio/buttplug-tampermonkey/raw/master/scripts/chaturbate-tips-buttplug.user.js
@@ -32,45 +32,183 @@
 // @exclude http*://*.*/privacy/*
 // @exclude http*://*.*/2257/*
 // @exclude http*://*.*/photo_videos/*
-// @require https://cdn.jsdelivr.net/npm/buttplug@0.12.2/dist/web/buttplug.min.js
-// @require https://raw.githubusercontent.com/buttplugio/buttplug-tampermonkey/master/utils/buttplug-tampermonkey-ui.js
+// @grant GM_addElement
+// @grant unsafeWindow
 // @copyright MIT
 // ==/UserScript==
 
-function newTipsOccurred(tips) {
-  console.log('Received new tips');
-  console.log(tips);
-}
+// Chaturbate forces script to fetch only from a set of certain sites that does not contain any of the cdns
+// So you will have to disable this protection of your browser
+// https://chrome.google.com/webstore/detail/always-disable-content-se/ffelghdomoehpceihalcnbmnodohkibj
+
+// Rather than using @require we do this,
+// this ensures that the last script in the doc will be loaded from the cdn
+// so when the `buttplug.min.js` tries to load the res of the code / wasm there won't be an issue
+GM_addElement(document.body, 'script', {
+  src: 'https://cdn.jsdelivr.net/npm/buttplug@1.0.17/dist/web/buttplug.min.js',
+  type: 'text/javascript'
+});
+
 window.addEventListener('load', async function() {
-  (function() {
-    let chatbox = $('.chat-box')[0];
-    let lastTipCount = 0;
-    chatbox.addEventListener('DOMNodeInserted', async function(event){
-      let tips = Array
-          .apply(null, $('.chat-box span.emoticonImage')
-                 .closest('div:not([data-nick]):has(span[style])'))
-          .filter(x => x.querySelector('span.emoticonImage').innerHTML.match(/tipped \d+ token(s)?/))
-          .map(x => {
-            return {
-              donation: x.firstChild.innerHTML,
-              amount: parseInt(x.lastChild.innerHTML.match(/tipped (\d+) token(s)?/)[1])
-            };
-          });
-      let newTips = tips.slice(lastTipCount);
-      lastTipCount = tips.length;
-      if (newTips.length) {
-        newTipsOccurred(newTips);
-        // window.buttplug_devices is defined in the buttplug-tampermonkey-ui.js
-        // file, and contains a list of all devices we've added. This is a
-        // Buttplug.ButtplugClientDevice object, API is at
-        // https://buttplug-js.docs.buttplug.io/classes/buttplugclientdevice.html
-        for (const device of window.buttplug_devices) {
-          if (device.AllowedMessages.includes("VibrateCmd")) {
-            await window.buttplug_devices[0].SendVibrateCmd(1.0);
-            setTimeout(() => window.buttplug_devices[0].SendVibrateCmd(0), newTips[0].amount * 100);
-          }
-        }
+  // if false
+  //   strength will be tip amount/rate (200 by default)
+  //     min 5%, max 100%
+  //   time will be tip amount / timeRate sec (10 by default)
+  //     no min / max
+  // if true
+  //   it will use the levels table
+  let levelsSet = false;
+  // set this if levelsSet is false
+  const rate = 200;
+  // set this if levelsSet is false
+  const minPower = 5;
+  // set this if levelsSet is false
+  const maxPower = 100;
+  // set this if levelsSet is false
+  const timeRate = 10;
+  // minToken - the lowest amount of token that trigger that level
+  // time - vibration length in seconds
+  // strength the power of the vibration %
+  const levels = [
+    {
+      minToken: 1,
+      time: 2,
+      strength: 20
+    },
+    {
+      minToken: 12,
+      time: 6,
+      strength: 40
+    },
+    {
+      minToken: 31,
+      time: 20,
+      strength: 40
+    },
+    {
+      minToken: 100,
+      time: 40,
+      strength: 70
+    },
+    {
+      minToken: 225,
+      time: 60,
+      strength: 100
+    },
+    {
+      minToken: 500,
+      time: 60,
+      strength: 100
+    },
+  ];
+  try {
+    // init bp
+    await Buttplug.buttplugInit();
+    // setup client
+    let buttplugClient = new Buttplug.ButtplugClient("Tutorial Client");
+    // holds the devices
+    // add devices
+    buttplugClient.addListener('deviceadded', async (device) => {
+      console.log('Connected devices:');
+      buttplugClient.Devices.forEach((device) => {
+        console.log('  - '+device.Name);
+      });
+      handleQueue();
+    });
+    buttplugClient.addListener('deviceremoved', async (device) => {
+      console.log('Connected devices:');
+      buttplugClient.Devices.forEach((device) => {
+        console.log('  - '+device.Name);
+      });
+    });
+
+    // use the embeded connector
+    const connector = new Buttplug.ButtplugEmbeddedConnectorOptions();
+    await buttplugClient.connect(connector);
+
+    let button = document.createElement("button");
+    button.innerHTML = "Click to connect device";
+    button.addEventListener('click', async () => {
+      await buttplugClient.startScanning();
+    });
+    document.getElementById("header").appendChild(button);
+
+    let button2 = document.createElement("button");
+    button2.innerHTML = "Clear queue, stop device";
+    button2.addEventListener('click', async () => {
+      if (timer) {
+        clearTimeout(timer);
+        timer = null;
+      }
+      queue.length = 0;
+      if (buttplugClient.Devices && buttplugClient.Devices.length) {
+        buttplugClient.Devices.forEach((device) =>{
+          device.vibrate(0);
+        });
       }
     });
-  })();
+    document.getElementById("header").appendChild(button2);
+
+    const queue = [];
+    let lastMessage = null;
+    let timer = null;
+
+    let chatbox = document.querySelector('.message-list');
+
+    // new message in the chat
+    chatbox.addEventListener('DOMNodeInserted', async function (event) {
+      const el = event.target;
+
+      if (lastMessage !== el && el.className.includes('msg-text') && el.querySelector('span.emoticonImage') && el.innerHTML.match(/tipped \d+ token(s)?/)) {
+        const amount = parseInt(el.innerHTML.match(/tipped (\d+) token(s)?/)[1]);
+        const newTip = {
+          amount: amount,
+          strength: levelsSet ?
+              levels.reduce((acc, level) => amount >= level.minToken ? level.strength : acc, 0) :
+              Math.min(Math.max(amount / rate * 100, minPower), maxPower),
+          time: levelsSet ?
+              levels.reduce((acc, level) => amount >= level.minToken ? level.time : acc, 0):
+              amount / timeRate,
+        };
+        addTipToQueue(newTip);
+        lastMessage = el;
+      }
+    });
+
+    async function handleQueue() {
+      console.log('queue', JSON.parse(JSON.stringify(queue)));
+      if (buttplugClient.Devices && buttplugClient.Devices.length > 0) {
+        const tip = queue.shift();
+        console.log('tip', tip);
+        if (tip) {
+          const strength = tip.strength;
+          const time = tip.time;
+
+          buttplugClient.Devices.forEach((device) =>{
+            device.vibrate(strength/100);
+          });
+
+          console.log('running for ' + time + ' sec, strength:' + strength);
+          timer = setTimeout(
+              () => handleQueue(),
+              time*1000
+          );
+        } else {
+          timer = null;
+          buttplugClient.Devices.forEach((device) =>{
+            device.vibrate(0);
+          });
+        }
+      }
+    }
+
+    function addTipToQueue(newTip) {
+      queue.push(newTip);
+      if (!timer) {
+        handleQueue();
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }, false);
